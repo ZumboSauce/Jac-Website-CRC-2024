5eed44fbe0d53653ffe6d55ed6415ce6b99e0650
Avery Berger <averyberger@icloud.com>
2024-03-16 17:38:56 -0500

    Finished so fucking much

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,33 +1,17 @@
-function Appear()
-{
-    document.getElementById("chest").style.display="none";
-    document.getElementById("cheststuff").style.animation="picswitch 2s linear";
-    document.getElementById("cheststuff").style.display="flex";
-    
-    if (document.getElementById("cheststuff").style.display=="flex"){
-        document.getElementById("on").style.display="flex";
-    }
-    
-    // console.log("nope")
+var slideIndex = 1;
+showDivs(slideIndex);
 
-    // if (document.getElementById("on").style.display == "none")
-    // {
-    //     document.getElementById("on").style.display ="flex";
-    //     console.log("Hi!")
-        
-    // }
-    // else
-    // {
-        
-    //     document.getElementById("on").style.display = "none";
-    //     console.log("hii!!!")
-    // }
+function plusDivs(n) {
+  showDivs(slideIndex += n);
 }
 
-function rotate(name){
-    var angle = 0;
-    document.getElementById(name).style.transform= "rotate3d(0, 1, 0, angle)";
-    angle++;
-    console.log("test",angle);
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
 }
-    
